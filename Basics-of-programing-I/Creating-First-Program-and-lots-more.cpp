bd0c7da31bebdee35c07a1fd91aa01636b59ac6e
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-07-31 19:54:51 -0700

    Creating-First-Program-and-lots-more

---
+++ Basics-of-programing-I/Creating-First-Program-and-lots-more.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // std::cout<<"Hello Duniya"<˜˜<std::endl;
+
+    // cout<<"Hello Duniya"<<endl;
+
+    // int a = 5;
+    // int b = 3;
+    // cout << (a <= b) << endl; // print 0 means false
+    // cout << (b <= a) << endl; // print 1 means true
+
+    // cout << !(a == 5);
+    // cout << (5)/((double)3);
+
+    // Arithmetic Operators
+
+    // cout << a + b << endl;
+    // cout << a - b << endl;
+    // cout << a * b << endl;
+    // cout << a % b << endl;
+    // cout << a / b << endl;
+
+    // double d = 5.7;
+    // int x = (int)d;
+    // cout<<"Value of X is "<<x<<endl;
+    // char ch = 20;
+    // cout<<ch<<endl;
+    // cout<< sizeof(ch);
+
+    // int num = 'A';
+    // cout<< num<<endl;
+
+    // long long a = 5;
+    // cout << sizeof(a)<<endl;
+    // int a = 5;
+    // cout<<"Value of a is :"<<a<<endl;
+    // char ch = 256;  // Warning generated // char ch 'a';
+    // cout<<"Value of ch is :"<<ch<<endl;
+    
+    // bool flag = true; // flag = 1
+    // cout<< "Value of flag is :"<<flag<<endl;
+
+    cout<<"Enter a number:"<<endl;
+    int a;
+    cin>>a; // taking input from here 
+
+    cout<<"Your enter value is: "<<a<<endl;
+
+
+    return 0;
+}
