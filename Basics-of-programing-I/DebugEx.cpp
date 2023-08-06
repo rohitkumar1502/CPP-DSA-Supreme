dbb41b522dd9324c00691e907b6fd6fa9f6a9134
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-08-05 19:44:29 -0700

    Debug and HW

---
+++ Basics-of-programing-I/DebugEx.cpp
@@ -0,0 +1,71 @@
+/*
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i=0;
+    i=i+1;
+    cout<<i<<endl;
+    i=i+1;
+    cout<<++i;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+void main() {
+    short i=2300, j=4322;
+    cout<<"i+j="<<(i+j);
+
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+
+int main() {
+    float l, b;
+    float P = 2*(l+b);
+    cout<<"perimeter="<<P;
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i=i+1)
+    {
+        for (int j = 0; j < n; j=j+1)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	bool isPrime = true;
+  for(int i=2;i<n;i++){
+      if(n%i == 0){
+          isPrime = false; break;
+      }
+  }
+	if(isPrime){
+		cout<<"Prime"<<endl;	
+	}
+	else{
+		cout<<"Not Prime"<<endl;	
+	}
+  return 0;
+}
+*/
---
+++ Basics-of-programing-I/HW.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // for(int row = 0; row<5; row++){
+    //     for(int col = 0; col < 5-row; col++)
+    //     {
+    //         cout<<"  ";
+    //     }
+    // }
+    // for(int row = 0; row <5; row++){
+    //     for(int col = 5; col<row+1; col++){
+    //         cout<<"* ";
+    //     }
+    // }
+    // cout<<endl;
+
+    /*
+    char ch = 234;   //warning: implicit conversion from 'int' to 'char' changes value from 234 to -22 [-Wconstant-conversion]
+                     //char ch = 234;
+    cout<<ch;
+    */
+
+    /*
+    short a = 32767;
+    short b = 10;
+    short sum = a+b;
+    cout<<sum;
+    */
+    /*
+
+     int a = 5;
+     char b = 'c';
+     int sum = a+b;
+     cout<<sum<<endl;
+     float f = 2.1 + sum;
+     cout<< f;
+     */
+   /*
+    float f = 2.7;
+    int a = 157;
+    int sub = a - f;
+    cout << sub;
+    */
+
+   int a = 25;
+   float b = 3.5;
+   float c = a-b;
+   cout << c;
+}
