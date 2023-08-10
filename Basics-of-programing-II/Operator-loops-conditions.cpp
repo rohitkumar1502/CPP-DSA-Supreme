9911da60a1854437ca3a5a10abbc2f7620a8e55f
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-08-10 00:47:02 -0700

    Operator

---
+++ Basics-of-programing-II/Operator-loops-conditions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool a = false;
+    bool b = true;
+    cout << (a | b)<<endl;
+    cout << (a & b)<<endl;
+    cout<< !a<<endl;
+    cout<< ~a<<endl;
+    cout << (a ^ b)<<endl;
+}
