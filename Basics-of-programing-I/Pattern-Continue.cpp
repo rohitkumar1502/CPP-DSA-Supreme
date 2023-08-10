a8cc93d8d7a020f5313b0a28a6aa4dea410e0bf2
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-08-09 22:41:29 -0700

    Pattern continue

--- Basics-of-programing-I/Pattern-Continue.cpp
+++ Basics-of-programing-I/Pattern-Continue.cpp
@@ -115,84 +115,169 @@ int main()
      cout<<endl;
     }
     */
-   /*
+    /*
+     int n;
+     cin >> n;
+     for (int row = 0; row < n; row++)
+     {
+         for (int col = 0; col < n - row; col++) //half pyramid
+         {
+             cout << "*";
+         }
+         for(int col =0; col<2*row+1; col++){ //full pyramid
+             cout<<" ";
+         }
+         for (int col = 0; col < n - row; col++) //half pyramid
+         {
+             cout << "*";
+         }
+         cout<<endl;
+
+
+
+
+     }
+      // inverse part
+     for(int row = 0; row<n; row++){
+             for(int col = 0;col<row+1; col++){
+                 cout<<"*";
+             }
+             for(int col=0; col<2*n-2*row-1; col++){
+                 cout<<" ";
+             }
+             for(int col = 0; col<row+1; col++){
+                 cout<<"*";
+             }
+             cout<<endl;
+         }
+     */
+    /*
     int n;
-    cin >> n;
-    for (int row = 0; row < n; row++)
-    {
-        for (int col = 0; col < n - row; col++) //half pyramid
-        {
-            cout << "*";
+    cin>>n;
+    for(int row = 0; row<n; row++){
+     for(int col=0; col<row+1; col++){
+         cout<<row+1;
+         if(row!=col){
+             cout<<"*";
+         }
+     }
+     cout<<endl;
+    }
+    for(int row=0; row<n; row++){
+     for(int col=0; col<n-row; col++){
+         cout<<n-row;
+         if(col!=n-row-1){
+             cout<<"*";
+         }
+
+     }
+     cout<<endl;
+    }
+    */
+    /*
+      int n;
+      cin>>n;
+      for(int row=0; row<n; row++){
+        int col;
+        for(col=0; col<row+1; col++){
+            int ans = col+1;
+            char ch = ans + 'A'-1;
+            cout<<ch;
+
         }
-        for(int col =0; col<2*row+1; col++){ //full pyramid
-            cout<<" ";
-        }
-        for (int col = 0; col < n - row; col++) //half pyramid
-        {
-            cout << "*";
+        col=col-1;
+        // reverse counting printing
+        for(; col>=1; col=col-1){
+            int ans = col;
+            char ch = ans +'A'-1;
+            cout<<ch;
+
         }
         cout<<endl;
-       
-        
-        
-       
-    }
-     // inverse part
-    for(int row = 0; row<n; row++){
-            for(int col = 0;col<row+1; col++){
-                cout<<"*";
-            }
-            for(int col=0; col<2*n-2*row-1; col++){
-                cout<<" ";
-            }
-            for(int col = 0; col<row+1; col++){
-                cout<<"*";
-            }
-            cout<<endl;
-        }
-    */
-   /*
+      }
+
+      */
+    /*
    int n;
-   cin>>n;
-   for(int row = 0; row<n; row++){
-    for(int col=0; col<row+1; col++){
-        cout<<row+1;
-        if(row!=col){
-            cout<<"*";
-        }
-    }
-    cout<<endl;
-   }
-   for(int row=0; row<n; row++){
-    for(int col=0; col<n-row; col++){
-        cout<<n-row;
-        if(col!=n-row-1){
-            cout<<"*";
-        }
-       
-    }
-    cout<<endl;
+   cin >> n;
+
+   for (int row = 0; row < n; row++)
+   {
+       int start= row+1;
+       for (int col = 0; col < row + 1; col++)
+       {
+           // cout<<row+col+1<<" ";
+           cout<<start<<" ";
+           start= start+1;
+       }
+       cout<<endl;
    }
    */
+    /*
+    int n;
+    cin>>n;
+    //spaces
+    for(int row =0; row<n; row++){
+     for(int col = 0; col<n-row; col++){
+         cout<<" ";
+     }
+     //counting
+     for(int col =0; col<row+1; col++){
+         cout<<row +col+1;
+     }
 
-  int n;
-  cin>>n;
-  for(int row=0; row<n; row++){
-    int col;
-    for(col=0; col<row+1; col++){
-        int ans = col+1;
-        char ch = ans + 'A'-1;
-        cout<<ch;
-       
+     // reverse
+     int reverse= row*2;
+     for(int col =0; col<reverse; col++){
+         cout<<reverse;
+         reverse=reverse-1;
+     }
+      cout<<endl;
     }
-    col=col-1;
-    // reverse counting printing
-    for(; col>=1; col=col-1){
-        int ans = col;
-        char ch = ans +'A'-1;
-        cout<<ch;
+    */
+   
 
+    int n;
+    cin >> n;
+    // spaces
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n - row; col++)
+        {
+            cout << " ";
+        }
+        // number with spaces
+        int start = 1;
+
+        for (int col = 0; col < 2 * row + 1; col++)
+        {
+            //    first row or last row
+            if (row == 0 || row == n - 1)
+            {
+                if (col % 2 == 0)
+                {
+                    // even
+                    cout << start;
+                    start = start + 1;
+                }
+                else{
+                    //odd
+                    cout<<" ";
+                }
+            }
+            else{
+                //first col
+                if(col ==0){
+                    cout<<1;
+                }
+                else if(col==2*row){
+                    cout<<row+1;
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+        }
+        cout << endl;
     }
-    cout<<endl;
-  }
 }
