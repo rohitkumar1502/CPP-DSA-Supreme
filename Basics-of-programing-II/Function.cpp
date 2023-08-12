9dc7f981f2fc16c14c81d6fb59c13a4daeada5e0
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-08-12 19:56:48 +0530

    Functions

---
+++ Basics-of-programing-II/Function.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void printName()
+{
+    int n;
+    cout << "Enter the value of n " << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Rohit Kumar" << endl;
+    }
+}
+
+void printNumber(int num)
+{
+    cout << num << endl;
+}
+
+int main()
+{
+    // Function call
+    // printName();
+    // int a = 5;
+    // cout << "Adress of a is : " << &a << endl;
+
+    // printNumber(a);
+
+    
+}
