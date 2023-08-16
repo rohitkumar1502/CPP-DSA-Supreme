4c31f753c71f88c0b2fa284f2e4ced9a2737154f
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-08-16 21:27:51 +0530

    arrays

---
+++ Arrays/Arrays_I.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /* // arrays declare
+     int arr[8];
+     cout<<arr<<endl;
+     cout<<&arr<<endl;
+     cout<<&arr[0]<<endl;
+     cout<<&arr[1]<<endl;
+     cout << "Arrays created sucessfully"<<endl;
+     */
+    /*
+    int arr[] = {1, 2, 3, 4, 5};
+    int brr[5] = {1, 2, 3, 3, 5};
+    int crr[7] = {1, 2, 3, 4, 5, 6, 7};
+    //int arr[4] = {1, 2, 3, 4, 5, 6, 7}; //error overloading
+    cout<<"Arrays initalized succesfully"<<endl;
+    */
+   /*
+   int arr[]= {1,3, 5, 7, 9};
+   for(int index =0; index<5; index++){
+    cout<<arr[index]<<" ";
+   }
+   */
+/*
+  int n;
+  cin>>n; // BAD practices
+  int arr[n];
+  */
+// Statics arrays
+
+
+ return 0;
+}
