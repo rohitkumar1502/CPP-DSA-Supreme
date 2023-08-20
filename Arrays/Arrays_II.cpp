fb90a2bcc5de07ecf45b15ab41bf1475b6aed092
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-08-20 11:35:33 +0530

    Vector

---
+++ Arrays/Arrays_II.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    /*
+        //Crating a vectors
+        vector<int>brr;
+        // vector<char>arr;
+        // int ans = (sizeof(arr)/sizeof(char));
+        // cout<<ans<<endl;
+        // cout<<arr.size()<<endl;
+        // cout<<arr.capacity()<<endl;
+
+        //Inesertion of array
+        brr.push_back(4);
+        brr.push_back(46);
+        for(int i = 0; i<brr.size(); i++){
+            cout<<brr[i]<<" "<<endl;
+
+        }
+    */
+    /*
+     int n;
+
+     cout << "Enter the value of n: ";
+     cin >> n;
+     vector<int> arr(n, -101);
+
+     for (int i = 0; i < arr.size(); i++)
+     {
+         cout << arr[i]<<" ";
+     }
+     cout<<endl;
+
+     cout << "Size of arr : " << arr.size() << endl;
+     cout << "Capacity of arr : " << arr.capacity() << endl;
+     */
+    /*
+    cout << "Printing crr" << endl;
+    vector<int> array;
+    array = {10, 20, 30, 40, 50};
+
+    for (int i = 0; i < array.size(); i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    */
+    return 0;
+
+}
