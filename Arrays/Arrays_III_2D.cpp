e675277c8a10836f94d0ccafa1c15e8c85546c96
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-08-20 17:49:02 +0530

    2D arrays and 2D Vectors

---
+++ Arrays/Arrays_III_2D.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+    int arr[3][3]={{1,2,3},
+    {4,5,6},
+    {7,8,9}};
+    cout<<arr[2][2]<<endl;
+    */
+   /*
+    int row, col;
+    cout << "Please Enter the Row and Col:: ";
+    cin >> row >> col;
+    vector<vector<int> >arr(row, vector<int>(col, 101));
+    for(int i =0; i<arr.size(); i++){
+        for(int j =0; j<arr[i].size();j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    */
+    
+}
