7a62b32eed6d12bc6109680bd8053cfa6ec81975
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-08-22 18:36:29 +0530

    Binary Search

--- 4.Searching and Sorting/Binary_Search.cpp
+++ 4.Searching and Sorting/Binary_Search.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include<vector>
 using namespace std;
 int binarySearch(int arr[], int size, int target)
 {
@@ -26,8 +28,41 @@ int binarySearch(int arr[], int size, int target)
     }
     return -1; // element not found
 }
+
+int firstOcurance(vector<int>v, int target){
+    int start = 0;
+    int end = v.size()-1;
+    int mid =start+ (end-start)/2;
+    int ans = -1;
+    while (start<=end)
+    {
+        if (v[mid]== target){
+             //ans store
+             ans = mid;
+             //left search
+             end = mid -1;
+
+
+        }
+        else if(target < v[mid]){
+            //search in left
+            end = mid-1;
+        }
+        else if(target > v[mid]){
+            //search on right
+            start = mid +1;
+        }
+      
+        mid =start+ (end-start)/2;
+     
+    }
+    return ans;
+    
+}
+
 int main()
 {
+    /*
     // Find the target element
     // element should be sorted then apply these concept
     int arr[] = {2, 3, 4, 8, 10, 12, 13};
@@ -42,4 +77,27 @@ int main()
     {
         cout << "Target Found at " << indexOftarget << " index" << endl;
     }
+  */
+ /*
+    //Found by using binarry in build search funcion 
+    int arr2[]={1,2,3,4,5,6,7};
+    // vector<int>arr2{1,2,3,4,5,6,7};
+    int size =7;
+    if(binary_search(arr2,arr2+size,7)){
+        cout<<"Find"<<endl;
+    }
+    else{
+        cout<<"Not Found";
+    }
+    */
+
+   //Find first occurance of the element
+   vector<int>v{1,3,3,3,3,3,3,4,4,4,4,6,7};
+   int target = 4;
+   int ans = firstOcurance(v, target);
+   cout<<"Ans is: "<<ans<<endl;
+   auto ans2 = lower_bound (v.begin(), v.end(),target);
+   cout<<"Ans 2 is: "<<ans2-v.begin()<<endl;
+
+ return 0;
 }
