fa257bd1e1ed7d83938337c52a1d497b5e9e5b8f
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-12-12 15:41:54 +0530

    revision

---
+++ 2.Basics-of-programing-II/week2conect.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+    //18+ then you guys able do vote
+    int age = 10;
+    (age>18) ? cout << "You are 18+, So you definitly able vote" : cout << "You are not 18+, So you are not able do vote sorry";
+   */
+    int num = 11;
+    (num % 2 == 0)? cout<<"Even ": cout<< "Odd";
+
+    return 0;
+}
