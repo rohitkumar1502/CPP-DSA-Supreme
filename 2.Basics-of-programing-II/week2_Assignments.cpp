bc6c92275bf8e46d6130f2adec0516edf75586f7
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-12-17 15:08:20 +0530

    Pattern Questions

--- 2.Basics-of-programing-II/week2_Assignments.cpp
+++ 2.Basics-of-programing-II/week2_Assignments.cpp
@@ -21,7 +21,7 @@ int main(){
     return 0;
 }
 */
-
+/*
 //Numeric half inverted pyramid
 #include<iostream>
 using namespace std;
@@ -44,4 +44,40 @@ int main(){
         
     } 
     return 0;
+}
+*/
+
+//Numeric palindrome equilateral pyramid
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int k = n;
+    for(int i = 0; i < n; i++){
+        int c = 1;
+        for(int j = 0; j < k; j++){
+            if(j<n-i-1){
+                cout<<" ";
+            }
+            else if(j<= n-1){
+                cout<<c;
+                c++;
+            }
+            else if(j == n){
+                c = c - 2;
+                cout<<c;
+                c--;
+            }
+            else{
+                cout<<c;
+                c--;
+            }
+        }
+        k++;
+        cout<<endl;
+    }
+
+
+
 }
