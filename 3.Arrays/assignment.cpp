40143057b145b1d9ce0dd520239b647af62e8420
Rohit Kumar <77092936+rohitkumar1502@users.noreply.github.com>
2023-12-20 21:29:59 +0530

    coding practise

---
+++ 3.Arrays/assignment.cpp
@@ -0,0 +1,33 @@
+//Move all the negative number to the left side of the arrays
+
+#include<iostream>
+using namespace std;
+void moveAllElementLeft(int *a, int n){
+    int l = 0,h =n-1;
+    while(l<h){
+        if(a[l]<0){
+            l++;
+        }
+        else if(a[h]>0){
+            h--;
+        
+        }
+        else{
+            swap(a[l],a[h]);
+        }
+    }
+
+}
+int main(){
+    int a[]={1,-4,3,-7,3,9};
+    int n = sizeof(a)/sizeof(int);
+    moveAllElementLeft(a,n);
+     for (int i = 0; i < n; i++)
+     {
+        /* code */
+        cout<<a[i]<<" ";
+     }
+     cout<<endl;
+     return 0;
+     
+}
