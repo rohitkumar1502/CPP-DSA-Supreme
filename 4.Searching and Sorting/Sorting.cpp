da7148325e753d1f2b64e5e4c52d3897d44418dd
Rohit Kumar <krohitgupta1502@gmail.com>
2024-09-20 17:43:06 +0530

    Sorting

--- 4.Searching and Sorting/Sorting.cpp
+++ 4.Searching and Sorting/Sorting.cpp
@@ -1,5 +1,5 @@
 //----- Selction sort ---------
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -73,6 +73,100 @@ int main()
   {
     cout << arr[i] << " ";
   }
+}
+*/
+
+//Exponential Search 
+/*
+#include<iostream>
+using namespace std;
+int binarySearch(int arr[], int start, int end, int find){
+  while(start<= end){
+    int mid = start + (end - start)/2;
+    if(arr[mid] == find){
+      return mid;
+    }
+    if(arr[mid]>find){
+      end = mid -1;
+    }
+    else{
+      start = mid +1;
+    }
+  }
+  return -1;
+}
+
+int exponentialSearch(int arr[], int n, int find){
+  if(arr[0] == find) return 0;
+  int i = 1;
+  while(i<n && arr[i]<=find){
+    i = i * 2; 
+    // i *= 2;
+    // i = i<<1;
+  }
+  return binarySearch(arr, i/2, min(i,n-1), find);
+}
+int main(){
+  int arr[] = {3,4,5,6,11,13,23,56,60,82};
+  int n = sizeof(arr)/sizeof(int);
+  int find = 84;
+  int result = exponentialSearch(arr, n ,find);
+  cout<<result;
+}
+
+*/
+// Unbounded Binary Search
+// array is in infinite ----brute force 
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+  int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+  int size = sizeof(arr)/sizeof(int);
+  int find = 15;
+  int i =0;
+  while(arr[i]<find){
+   i = i+1;
+  }
+  cout<<i<<endl;
+}
+
+*/
+
+// --- Optimize Approach---
+
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int start, int end, int find){
+  while(start<=end){
+    int mid = start + (end - start)/2;
+    if(arr[mid]== find){
+      return mid;
+    }
+    else if(arr[mid]> find){
+      end = mid -1;
+    }
+    else{
+      start = mid +1;
+    }
+  }
+  return -1;
+}
+int main(){
+  int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+  int size = sizeof(arr)/sizeof(int);
+  int find = 1;
+  int i =0;
+  int j =1;
+  while(arr[j]<find){
+    i = j;
+    j = j*2;
+  }
+  int start = i, end = j;
+  int result = binarySearch(arr, start, end, find);
+  cout<<result;
 }
 
 
