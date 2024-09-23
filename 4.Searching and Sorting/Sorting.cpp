9bc984f939de645efb35a124ed4ddadc6a477b87
Rohit Kumar <krohitgupta1502@gmail.com>
2024-09-23 13:38:40 +0530

    EKO Spoj

--- 4.Searching and Sorting/Sorting.cpp
+++ 4.Searching and Sorting/Sorting.cpp
@@ -17,7 +17,7 @@ void selctionSort(int arr[], int n)
     }
     // int temp = arr[min];
     // arr[min] = arr[i];
-    // arr[i] = temp; 
+    // arr[i] = temp;
     swap(arr[i], arr[min]);
   }
 }
@@ -76,7 +76,7 @@ int main()
 }
 */
 
-//Exponential Search 
+// Exponential Search
 /*
 #include<iostream>
 using namespace std;
@@ -100,7 +100,7 @@ int exponentialSearch(int arr[], int n, int find){
   if(arr[0] == find) return 0;
   int i = 1;
   while(i<n && arr[i]<=find){
-    i = i * 2; 
+    i = i * 2;
     // i *= 2;
     // i = i<<1;
   }
@@ -116,7 +116,7 @@ int main(){
 
 */
 // Unbounded Binary Search
-// array is in infinite ----brute force 
+// array is in infinite ----brute force
 /*
 #include<iostream>
 using namespace std;
@@ -135,7 +135,7 @@ int main(){
 */
 
 // --- Optimize Approach---
-
+/*
 #include<iostream>
 using namespace std;
 
@@ -169,4 +169,201 @@ int main(){
   cout<<result;
 }
 
+*/
+/*
+You have n books, each with arr[i] a number of pages. m students need to be allocated contiguous books, with each student getting at least one book.
+Out of all the permutations, the goal is to find the permutation where the sum of the maximum number of pages in a book allotted to a student should be the minimum, out of all possible permutations.
+Note: Return -1 if a valid assignment is not possible, and allotment should be in contiguous order (see the explanation for better understanding).
+Examples:
+Input: n = 4, arrl] = [12, 34, 67, 90], m = 2
+Output: 113
+Explanation: Allocation can be done in following ways:
+{12} and {34, 67, 90} Maximum Pages = 191
+{12, 34} and {67, 90} Maximum Pages = 157
+{12, 34, 67} and {90} Maximum Pages =113.
+Therefore, the minimum of these cases is 113, which is selected as the output.
+*/
+/*  //Very Imprtant Question
+class Solution {
+  public:
+    bool isPosible(int n, int arr[], int m, int mid){
+        int pageSum = 0;
+        int count = 1;
+        for(int i =0; i< n; i++){
+            if(arr[i]>mid){
+                return false;
+            }
+            if(pageSum + arr[i]>mid){
+                count++;
+                pageSum = arr[i];
+                if(count> m){
+                    return false;
+                }
+            }
+            else{
+                pageSum += arr[i];
+            }
+        }
+        return true;
+    }
 
+     long long findPages(int n, int arr[], int m) {
+        // code here
+        if(n<m) return -1;
+        int start = 0;
+        int end = accumulate(arr, arr + n, 0);
+        int ans = -1;
+        while(start<= end){
+            int mid = (start+end)>>1; // if we do right shift then we assume that we dived by 2
+            if(isPosible(n, arr, m , mid)){
+                ans = mid;
+                end = mid -1;
+            }
+            else{
+                start = mid +1;
+            }
+        }
+        return ans;
+
+    }
+};
+
+Dry Run
+Input
+Let’s assume the following input for clarity:
+
+arr[] = {12, 34, 67, 90} (number of pages in each book)
+n = 4 (number of books)
+m = 2 (number of students)
+The goal is to minimize the maximum number of pages a student will have to read.
+
+findPages() Execution
+Initialization:
+
+start = 0 (minimum possible pages a student can read).
+end = accumulate(arr, arr + n, 0) which gives end = 12 + 34 + 67 + 90 = 203
+ (maximum pages a student may have to read if only one student is assigned all books).
+ans = -1 (stores the final answer).
+Binary Search:
+
+mid = (start + end) >> 1 = (0 + 203) >> 1 = 101 (we divide the current range in half).
+Call isPosible() with mid = 101:
+
+Step 1: Initialize pageSum = 0 and count = 1 (we start with the first student).
+Step 2: For i = 0, arr[0] = 12, add 12 to pageSum, now pageSum = 12.
+Step 3: For i = 1, arr[1] = 34, add 34 to pageSum, now pageSum = 46.
+Step 4: For i = 2, arr[2] = 67, if we add 67 to pageSum, 46 + 67 = 113, which exceeds mid. So,
+allocate the third book to a new student (increase count = 2) and reset pageSum = 67.
+Step 5: For i = 3, arr[3] = 90, if we add 90 to pageSum, 67 + 90 = 157,
+which again exceeds mid. So, allocate the fourth book to a new student (increase count = 3),
+but now count > m (i.e., more students are needed than available). Hence, return false.
+Back to findPages():
+
+Since isPosible(101) returned false, update start = mid + 1 = 101 + 1 = 102.
+Repeat Binary Search:
+
+Next iteration, mid = (start + end) >> 1 = (102 + 203) >> 1 = 152.
+Call isPosible() with mid = 152.
+Steps 1–5 will proceed similarly, and this time isPosible() returns true. Hence, update ans = mid = 152, and
+end = mid - 1 = 152 - 1 = 151.
+Continue Binary Search:
+
+This binary search continues until the range [start, end] is exhausted.
+The final ans will give the minimum possible maximum number of pages a student has to read.
+Output
+The minimum number of pages will be the final value of ans, which will be the optimal page
+ distribution for the students.
+Based on the binary search process, the output for this example might be around 113.
+
+*/
+
+/*
+EKO - Eko
+#binary-search
+https://www.spoj.com/problems/EKO/
+
+Lumberjack Mirko needs to chop down M metres of wood. It is an easy job for him since he has a nifty new woodcutting machine that can take down forests like wildfire. 
+However, Mirko is only allowed to cut a single row of trees.
+
+Mirko‟s machine works as follows: Mirko sets a height parameter H (in metres), and the machine raises a giant sawblade to that height and cuts off all tree parts higher 
+than H (of course, trees not higher than H meters remain intact). Mirko then takes the parts that were cut off. For example, if the tree row contains trees with heights of 
+20, 15, 10, and 17 metres, and Mirko raises his sawblade to 15 metres, the remaining tree heights after cutting will be 15, 15, 10, and 15 metres, respectively, 
+while Mirko will take 5 metres off the first tree and 2 metres off the fourth tree (7 metres of wood in total).
+
+Mirko is ecologically minded, so he doesn‟t want to cut off more wood than necessary. That‟s why he wants to set his sawblade as high as possible. 
+Help Mirko find the maximum integer height of the sawblade that still allows him to cut off at least M metres of wood.
+
+Input
+The first line of input contains two space-separated positive integers, N (the number of trees, 1 ≤ N ≤ 1 000 000) and M (Mirko‟s required wood amount, 1 ≤ M ≤ 2 000 000 000).
+
+The second line of input contains N space-separated positive integers less than 1 000 000 000, the heights of each tree (in metres). The sum of all heights will exceed M, 
+thus Mirko will always be able to obtain the required amount of wood.
+
+Output
+The first and only line of output must contain the required height setting.
+
+Example
+Input:
+4 7
+20 15 10 17
+
+Output:
+15
+Input:
+5 20
+4 42 40 26 46
+
+Output:
+36
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool isPosibleSol(vector<long long int> trees,long long int m, long long int mid){
+  long long int woodCollected = 0;
+  int n = trees.size();
+  for(int i = 0;i < n; i++){
+    if(trees[i] > mid){
+      woodCollected += trees[i] - mid;
+    }
+   
+  }
+   return woodCollected >= m;
+}
+
+long long int maxBledSawHight(vector<long long int> trees, long long int m){
+  long long int start = 0, end, ans = -1;
+  end = *max_element(trees.begin(), trees.end());
+  while(start<= end){
+    long long int mid = start + (end - start)/2;
+    if(isPosibleSol(trees, m, mid)){
+      ans = mid;
+      start = mid + 1;
+    }
+    else{
+      end = mid -1;
+    }
+  }
+  return ans;
+}
+int main(){
+  long long int n, m;
+  cin>>n>>m;
+
+  vector<long long int> trees;
+  cout<<"Enter the height of tress: ";
+  while(n--){
+    long long int hight;
+    cin>>hight;
+    trees.push_back(hight);
+  }
+
+  cout<<maxBledSawHight(trees, m);
+
+
+
+ return 0;
+}
